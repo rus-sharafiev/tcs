589542b8d58440ab5ad50834284a2d48cbe1700a
Rus Sharafiev <srr@live.ru>
2023-06-04 23:55:52 +0300

    Add album

--- src/components/Slides.tsx
+++ src/components/Slides.tsx
@@ -2,43 +2,61 @@ import type SwiperCore from 'swiper'
 import { useDropzone } from "react-dropzone"
 import { Navigation, Pagination } from "swiper"
 import { Swiper, SwiperSlide } from 'swiper/react'
-import React, { useEffect, useState } from "react"
-import { setFiles } from "../store/reducers/filesSlice"
+import { useEffect, useState } from "react"
 import { useAppDispatch, useAppSelector } from "../store"
-import { setImagePreview } from "../utils/setImagePreview"
 // @mui
 import Modal from "@mui/material/Modal"
 import IconButton from "@mui/material/IconButton"
 import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded'
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded'
+import { addFileToSlide } from '../store/reducers/filesSlice'
+import { ImageFileObject, getImageObject } from '../utils/getImageObject'
+import Divider from '@mui/material/Divider'
 
 // ----------------------------------------------------------------------
 
-interface SlidesProps {
+export interface SlidesProps {
     setSwiper?: React.Dispatch<React.SetStateAction<SwiperCore | null>>
     type: 'gallery' | 'template' | 'background'
 }
 
+export interface Slide {
+    left: ImageFileObject | undefined
+    right: ImageFileObject | undefined
+}
+
 export const Slides: React.FC<SlidesProps> = ({ setSwiper, type }) => {
-    const files = useAppSelector(state => state.files[type])
+    const slides = useAppSelector(state => state.files.slides)
     const dispatch = useAppDispatch()
     const [activeSlide, setActiveSlide] = useState<number>(0)
+    const [openedImage, setOpenedImage] = useState<string>()
     const [openModal, setOpenModal] = useState(false)
 
-    const onDrop = async (acceptedFiles: File[]) => {
-        const filesWithPreview = await Promise.all(acceptedFiles.map(file => setImagePreview(file)))
-        dispatch(setFiles({ files: filesWithPreview, type }))
+    const handleLeftDrop = async (acceptedFiles: File[]) => {
+        const filesWithPreview = await Promise.all(acceptedFiles.map(file => getImageObject(file)))
+        dispatch(addFileToSlide({ file: filesWithPreview[0], index: activeSlide, side: 'left' }))
     }
 
-    const { getRootProps, isDragActive, getInputProps } = useDropzone({ onDrop, noClick: true })
+    const handleRightDrop = async (acceptedFiles: File[]) => {
+        const filesWithPreview = await Promise.all(acceptedFiles.map(file => getImageObject(file)))
+        dispatch(addFileToSlide({ file: filesWithPreview[0], index: activeSlide, side: 'right' }))
+    }
+
+    const handleOpenImage = (data: string) => {
+        setOpenedImage(data)
+        setOpenModal(true)
+    }
+
+    const left = useDropzone({ onDrop: handleLeftDrop, noClick: true })
+    const right = useDropzone({ onDrop: handleRightDrop, noClick: true })
 
     useEffect(() => {
         setActiveSlide(0)
     }, [type])
 
     return (
-        <div id="slider-container" {...getRootProps()}>
-            {files.length > 0
+        <div id="slider-container">
+            {slides.length > 0
                 ? <>
                     <Swiper
                         modules={[Navigation, Pagination]}
@@ -49,10 +67,28 @@ export const Slides: React.FC<SlidesProps> = ({ setSwiper, type }) => {
                         }}
                         onRealIndexChange={(element) => setActiveSlide(element.activeIndex)}
                     >
-                        {files.map((img, index) =>
-                            <SwiperSlide key={'slide-' + index}>
-                                <div className="img-container" onClick={() => setOpenModal(true)}>
-                                    <img src={img.preview} alt={img.name} />
+                        {slides.map((slide, index) =>
+                            <SwiperSlide key={'slide-' + index} className='slide'>
+                                <div className='album'>
+                                    <div className='left' {...left.getRootProps()}>
+                                        <div>{slide.left &&
+                                            <img
+                                                src={slide.left.data as string}
+                                                alt={slide.left.name}
+                                                onClick={() => slide.left && handleOpenImage(slide.left.data as string)}
+                                            />}
+                                        </div>
+                                    </div>
+                                    <Divider orientation="vertical" flexItem />
+                                    <div className='right' {...right.getRootProps()}>
+                                        <div>{slide.right &&
+                                            <img
+                                                src={slide.right.data as string}
+                                                alt={slide.right.name}
+                                                onClick={() => slide.right && handleOpenImage(slide.right.data as string)}
+                                            />}
+                                        </div>
+                                    </div>
                                 </div>
                             </SwiperSlide>
                         )}
@@ -69,15 +105,13 @@ export const Slides: React.FC<SlidesProps> = ({ setSwiper, type }) => {
                 </>
                 :
                 <div className="placeholder">Довавьте фото</div>}
-            <input {...getInputProps()} />
-            {isDragActive && <div className="drag-photo">Перетащите фотографии сюда</div>}
 
             <Modal
                 open={openModal}
                 onClose={() => setOpenModal(false)}
             >
                 <div className='modal'>
-                    {files.length > 0 && files[activeSlide] && <img src={files[activeSlide].preview} alt='image' />}
+                    {openedImage && <img src={openedImage} alt='image' />}
                 </div>
             </Modal>
         </div >
